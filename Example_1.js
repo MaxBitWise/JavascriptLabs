d66892f4992b09a65e79482f4f6efaaae9edc584
Max <54585449+MaxBitWise@users.noreply.github.com>
2019-09-26 20:10:48 +0500

    Lab1

---
+++ Example_1.js
@@ -0,0 +1,7 @@
+/*alert("Hello");
+alert("World!");
+let years = prompt('How are you old', 100);
+alert('You ' + years + ' years!');
+*/
+let isAdmin = confirm("Are you administrator?");
+alert(isAdmin);
